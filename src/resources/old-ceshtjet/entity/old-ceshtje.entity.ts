3c6bfbb78f0014f82f1b1398e53227eaaf673e99
Julian Cuni <juliancuni@gmail.com>
2021-05-14 18:47:03 +0200

    Migration to Mysql Success. DeleteResult UpdateResult OK!

--- src/resources/old-ceshtjet/entity/old-ceshtje.entity.ts
+++ src/resources/old-ceshtjet/entity/old-ceshtje.entity.ts
@@ -1,9 +1,13 @@
-import { Column, Entity, ObjectIdColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, ObjectIdColumn, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: 'old-ceshtjet' })
 export class OldCeshtjet {
-    @ObjectIdColumn() id: string;
-    @Column({ type: 'string', unique: false }) emri: string;
-    @Column({ type: 'string', unique: false }) mbiemri: string;
-    @Column({ type: 'date' }) createdAt: any;
+    // @ObjectIdColumn() id: string;
+    @PrimaryGeneratedColumn('uuid') public id: string;
+    // @Column({ type: 'string', unique: false }) emri: string;
+    @Column() emri: string;
+    // @Column({ type: 'string', unique: false }) mbiemri: string;
+    @Column() mbiemri: string;
+    // @Column({ type: 'date' }) createdAt: any;
+    @CreateDateColumn() createdAt: any;
 }
